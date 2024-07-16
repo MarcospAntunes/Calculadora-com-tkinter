7feced4c737f036bf4b7b43ddd681d8d5b4dc689
Marcospantunes <marcospantunes203@gmail.com>
2024-07-16 20:51:10 -0300

    refactor: refatorado código dos botões
    Refatorado código dos botões para diminuir o tamanho do código e torna-lo mais legível.

--- src/components/buttons/buttons.py
+++ src/components/buttons/buttons.py
@@ -2,89 +2,29 @@ from tkinter import *
 from ...utils import adicionaValores
 
 def buttons(window:Tk):
+    buttonsProps = [
+    { "text": "%", "column": 0, "row": 2 },
+    { "text": "CE", "column": 1, "row": 2 },
+    { "text": "C", "column": 2, "row": 2 },
+    { "text": "⊲", "column": 3, "row": 2 },
+    { "text": "1/x", "column": 0, "row": 3 },
+    { "text": "x^2", "column": 1, "row": 3 },
+    { "text": "√x", "column": 2, "row": 3 },
+    { "text": "÷", "column": 3, "row": 3 },
+    { "text": "9", "column": 0, "row": 4 },
+]
     buttons = Frame(window, padx=50).grid(column=0, row=1)
-    porcentagemButton = Button(
-        buttons, 
-        text="%", 
-        justify="center", 
-        padx=5, 
-        pady=5, 
-        command=lambda: adicionaValores(window, porcentagemButton)
-    )
-    porcentagemButton.grid(column=0, row=2)
 
-    deletAllButton = Button(
-        buttons, 
-        text="CE", 
-        justify="center", 
-        padx=5, pady=5, 
-        command=lambda: adicionaValores(window, deletAllButton)
-    )
-    deletAllButton.grid(column=1, row=2)
-
-    deletCurrentNumberButton = Button(
-        buttons, 
-        text="C", 
-        justify="center", 
-        padx=5, 
-        pady=5, 
-        command=lambda: adicionaValores(window, deletCurrentNumberButton)
-    )
-    deletCurrentNumberButton.grid(column=2, row=2)
-
-    deletButton = Button(
-        buttons, 
-        text="⊲", 
-        justify="center", 
-        padx=5, pady=5, 
-        command=lambda: adicionaValores(window, deletButton)
-    )
-    deletButton.grid(column=3, row=2)
-
-    fractionButton = Button(
-        buttons, 
-        text="1/x", 
-        justify="center", 
-        padx=5, 
-        pady=5, 
-        command=lambda: adicionaValores(window, fractionButton)
-    )
-    fractionButton.grid(column=0, row=3)
-
-    numberPowerTwoButton = Button(
-        buttons, 
-        text="x^2", 
-        justify="center", 
-        padx=5, pady=5, 
-        command=lambda: adicionaValores(window, numberPowerTwoButton)
-    )
-    numberPowerTwoButton.grid(column=1, row=3)
-
-    squareRootButton = Button(
-        buttons, 
-        text="√x", 
-        justify="center", 
-        padx=5, 
-        pady=5, 
-        command=lambda: adicionaValores(window, squareRootButton)
-    )
-    squareRootButton.grid(column=2, row=3)
-
-    divideButton = Button(
-        buttons, 
-        text="÷", 
-        justify="center", 
-        padx=5, 
-        pady=5, 
-        command=lambda: adicionaValores(window, divideButton)
-    )
-    divideButton.grid(column=3, row=3)
-    numberNine = Button(
-        buttons, 
-        text="9", 
-        justify="center", 
-        padx=5, 
-        pady=5, 
-        command=lambda: adicionaValores(window, numberNine)
-    )
-    numberNine.grid(column=0, row=4)
+    for buttonProp in buttonsProps:
+        currentButton = Button(
+            buttons,
+            text=buttonProp["text"],
+            justify="center",
+            padx=5,
+            pady=5,
+            command=lambda: adicionaValores(window, currentButton)
+        )
+        currentButton.grid(
+            column=buttonProp["column"], 
+            row=buttonProp["row"]
+        )
